6877d9f2912d20e11d4767a70aee5472e79abffd
hmctsdtschallenge <hmctsdtschallenge@hotmail.com>
2025-04-25 09:46:55 +0100

    feat: implement blank onCreateBtnClick and onBackBtnClick methods in CreateTaskClickHandler

--- app/src/main/java/com/dts/casemanagerfrontendandroid/ui/createtask/CreateTaskClickHandler.java
+++ app/src/main/java/com/dts/casemanagerfrontendandroid/ui/createtask/CreateTaskClickHandler.java
@@ -1,4 +1,16 @@
 package com.dts.casemanagerfrontendandroid.ui.createtask;
 
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
 public class CreateTaskClickHandler {
+    public void onCreateBtnClicked(View view) {
+    }
+
+    public void onBackBtnClicked(View view) {
+    }
 }
+
+
