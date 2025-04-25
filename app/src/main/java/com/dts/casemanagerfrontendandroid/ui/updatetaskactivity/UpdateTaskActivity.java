bc652573ed7f5a208776b66d916ae2aa72a8b9a2
hmctsdtschallenge <hmctsdtschallenge@hotmail.com>
2025-04-25 12:49:45 +0100

    refactor: disable edgeToEdge in Activities to stop top android icons blocking headers

--- app/src/main/java/com/dts/casemanagerfrontendandroid/ui/updatetaskactivity/UpdateTaskActivity.java
+++ app/src/main/java/com/dts/casemanagerfrontendandroid/ui/updatetaskactivity/UpdateTaskActivity.java
@@ -26,7 +26,7 @@ public class UpdateTaskActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_update_task);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
