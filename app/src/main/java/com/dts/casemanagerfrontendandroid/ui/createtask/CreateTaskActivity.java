1bf07c0ed323715acffe62f6074eba9db8d44077
hmctsdtschallenge <hmctsdtschallenge@hotmail.com>
2025-04-25 10:49:10 +0100

    feat: implement CreateTaskClickActivity Task binding, and click handler

--- app/src/main/java/com/dts/casemanagerfrontendandroid/ui/createtask/CreateTaskActivity.java
+++ app/src/main/java/com/dts/casemanagerfrontendandroid/ui/createtask/CreateTaskActivity.java
@@ -7,10 +7,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.dts.casemanagerfrontendandroid.R;
+import com.dts.casemanagerfrontendandroid.databinding.ActivityCreateTaskBinding;
+import com.dts.casemanagerfrontendandroid.model.Task;
+import com.dts.casemanagerfrontendandroid.ui.mainactivity.MainActivityViewModel;
 
 public class CreateTaskActivity extends AppCompatActivity {
+    private ActivityCreateTaskBinding binding;
+    private CreateTaskClickHandler handler;
+    private Task task;
+    private final String TAG = "CreateTaskActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +31,20 @@ public class CreateTaskActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        task = new Task();
+
+        binding = DataBindingUtil.setContentView(
+                this,
+                R.layout.activity_create_task
+        );
+
+        MainActivityViewModel viewModel = new ViewModelProvider(this)
+                .get(MainActivityViewModel.class);
+
+        handler = new CreateTaskClickHandler(task, this, viewModel);
+
+        binding.setTask(task);
+        binding.setClickHandler(handler);
     }
 }
