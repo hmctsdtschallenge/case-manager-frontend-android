4b4f8932d261d90e86ce35fb915795348220639b
hmctsdtschallenge <hmctsdtschallenge@hotmail.com>
2025-04-25 10:48:21 +0100

    feat: implement CreateTaskClickHandler constructor, and methods for onCreateBtnClicked and onBackBtnClicked

--- app/src/main/java/com/dts/casemanagerfrontendandroid/ui/createtask/CreateTaskClickHandler.java
+++ app/src/main/java/com/dts/casemanagerfrontendandroid/ui/createtask/CreateTaskClickHandler.java
@@ -1,16 +1,57 @@
 package com.dts.casemanagerfrontendandroid.ui.createtask;
 
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import com.dts.casemanagerfrontendandroid.model.Task;
+import com.dts.casemanagerfrontendandroid.model.TaskDTO;
+import com.dts.casemanagerfrontendandroid.ui.mainactivity.MainActivity;
+import com.dts.casemanagerfrontendandroid.ui.mainactivity.MainActivityViewModel;
+
 public class CreateTaskClickHandler {
+    Task task;
+    Context context;
+    MainActivityViewModel mainActivityViewModel;
+    String TAG = "CreateTaskClickHandler";
+
+    public CreateTaskClickHandler(Task task, Context context, MainActivityViewModel mainActivityViewModel) {
+        this.task = task;
+        this.context = context;
+        this.mainActivityViewModel = mainActivityViewModel;
+    }
+
     public void onCreateBtnClicked(View view) {
+        if(
+                task.getTitle() == null || task.getTitle().isBlank() ||
+                task.getDescription() == null || task.getDescription().isEmpty() ||
+                task.getStatus() == null || task.getStatus().isEmpty() ||
+                task.getCreatedDate() == null || task.getCreatedDate().isEmpty() ||
+                task.getDueDate() == null || task.getDueDate().isEmpty()
+        ) {
+            Toast.makeText(context,"Task fields cannot be empty",Toast.LENGTH_SHORT).show();
+            Log.i(TAG, "onSubmitBtnClicked: invalid task submitted");
+        } else {
+            Intent intent = new Intent(context, MainActivity.class);
+            TaskDTO taskDTO = new TaskDTO(
+                    task.getTitle(),
+                    task.getDescription(),
+                    task.getStatus(),
+                    task.getCreatedDate(),
+                    task.getDueDate());
+            mainActivityViewModel.postTask(taskDTO);
+            context.startActivity(intent);
+            Log.i(TAG, "onSubmitBtnClicked: valid task submitted");
+        }
     }
 
     public void onBackBtnClicked(View view) {
+        Intent intent = new Intent(context, MainActivity.class);
+        context.startActivity(intent);
     }
+
 }
 
 
